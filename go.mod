3071dff31c78aad1b6659f0025f7daa2e1b4dff3
mick4711 <mick4711@gmail.com>
2023-10-26 16:06:37 +0100

    remove students answers and create starter main file

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/mick4711/quiz
+
+go 1.21.3
---
+++ main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"encoding/csv"
+	"fmt"
+	"io"
+	"log"
+	"os"
+)
+
+func main() {
+	file, err := os.Open("problems.csv")
+	if err != nil {
+		log.Fatal(err)
+	}
+	r := csv.NewReader(file)
+	for {
+		record, err := r.Read()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println(record)
+	}
+}
